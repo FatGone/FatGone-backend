44b675b7fed1cc734e59a2dc80117ad671b8f83c
Daniel <daniel.oblak99@gmail.com>
2023-05-03 00:37:28 +0200

    JwtStrategy implementation

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common/decorators/core/injectable.decorator';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { User } from 'src/users/models/user.model';
+import { UsersService } from 'src/users/services/users.service';
+import { JwtPayload } from '../models/jwt_payload.dto';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private configService: ConfigService,
+    private usersService: UsersService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: any): Promise<User> {
+    return this.usersService.findById(payload.payload.id);
+  }
+}
