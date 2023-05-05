eff64f7f2e8e721a6c3d84f4dd4d348dedbf64f5
Daniel <daniel.oblak99@gmail.com>
2023-05-05 09:37:44 +0200

    Implemented UserDetailsController

---
+++ src/user_details/controllers/user_details.controller.ts
@@ -0,0 +1,64 @@
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Param,
+  Patch,
+} from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { UserDetailsService } from '../services/user_details.service';
+import { UserDetailsDto } from '../dto/user_details.dto';
+import { UserDetails } from '../models/user_details.model';
+import { Secured } from 'src/auth/decorators/secured.decorator';
+import { CardDetailsDto } from '../dto/card_details.dto';
+import { CardDetails } from 'src/payments/models/card_details.model';
+import { CurrentAccount } from 'src/auth/accounts/decorators/account.decorator';
+import { Account } from 'src/auth/accounts/model/account.model';
+
+@ApiTags('user-details')
+@Controller('user-details')
+export class UserDetailsController {
+  constructor(private userDetailsService: UserDetailsService) {}
+
+  @ApiCreatedResponse({
+    type: UserDetails,
+    description: 'Updated product successfully',
+  })
+  @ApiBadRequestResponse({
+    type: BadRequestException,
+    description: 'Body does not match defined schema',
+  })
+  @ApiNotFoundResponse()
+  @Secured()
+  @Patch()
+  async patch(
+    @CurrentAccount() account,
+    // @Param('id') id: number,w
+    @Body() userDto: UserDetailsDto,
+  ): Promise<Account> {
+    return await this.userDetailsService.patch(account.id, userDto);
+  }
+
+  @ApiCreatedResponse({
+    type: String,
+    description: 'Updated user card-details successfully',
+  })
+  @ApiBadRequestResponse({
+    type: BadRequestException,
+    description: 'Body does not match defined schema',
+  })
+  @ApiNotFoundResponse()
+  @Secured()
+  @Patch('/:id/card-details')
+  async patchCardDetails(
+    @Param('id') id: number,
+    @Body() cardDetailsDto: CardDetailsDto,
+  ): Promise<CardDetails> {
+    return await this.userDetailsService.patchCardDetails(id, cardDetailsDto);
+  }
+}
