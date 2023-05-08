d57be35a161f2456264d968f4abcdf052ba616a0
Daniel Obłąk <daniel.oblak@codeclusive.io>
2023-05-08 21:42:09 +0200

    Moved and renamed PaymentsService => CardDetailsService to CardDetails feature folder

--- src/payments/services/payments.service.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { Repository } from 'typeorm';
-
-@Injectable()
-export class PaymentsService {
-  constructor(private readonly paymentsRepository: Repository<undefined>) {}
-}
