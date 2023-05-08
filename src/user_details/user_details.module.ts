f1123e8abb2ca4318608b288d1440ceaa25aa818
Daniel Obłąk <daniel.oblak@codeclusive.io>
2023-05-08 21:40:27 +0200

    Code formatting in UserDetailsModule, removed commented code

--- src/user_details/user_details.module.ts
+++ src/user_details/user_details.module.ts
@@ -4,14 +4,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserDetails } from './models/user_details.model';
 import { UserDetailsService } from './services/user_details.service';
 import { UserDetailsController } from './controllers/user_details.controller';
-import { CardDetails } from 'src/payments/models/card_details.model';
 import { Account } from 'src/accounts/model/account.model';
+import { CardDetails } from 'src/card_details/models/card_details.model';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([UserDetails, CardDetails, Account]),
-    // TypeOrmModule.forFeature([Favourites]),
-  ],
+  imports: [TypeOrmModule.forFeature([UserDetails, CardDetails, Account])],
   controllers: [UserDetailsController],
   providers: [UserDetailsService],
   exports: [UserDetailsService],
