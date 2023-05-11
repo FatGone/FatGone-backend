b2a3202e920a3d28c6b86cafde16d7ac6d3622c3
Daniel Obłąk <daniel.oblak@codeclusive.io>
2023-05-11 16:33:10 +0200

    Removed class validator on LoginDto

--- src/auth/models/login.dto.ts
+++ src/auth/models/login.dto.ts
@@ -1,11 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsString } from 'class-validator';
 
 export class LoginDTO {
   @ApiProperty()
-  @IsEmail()
   email: string;
   @ApiProperty()
-  @IsString()
   password: string;
 }
