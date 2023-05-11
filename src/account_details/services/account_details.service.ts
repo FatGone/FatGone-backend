a9a26a70dc707fd3ec95a2a7bdf04a7fa118de47
Daniel Obłąk <daniel.oblak@codeclusive.io>
2023-05-11 16:30:39 +0200

    Implemented get method for AccountDetailsService, implemented SaveAccountDetails method and SoftRemove

--- src/account_details/services/account_details.service.ts
+++ src/account_details/services/account_details.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Inject,
+  Injectable,
+  NotFoundException,
+  forwardRef,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm/repository/Repository';
 import { AccountDetails } from '../models/account_details.model';
@@ -11,8 +16,18 @@ export class AccountDetailsService {
   constructor(
     @InjectRepository(AccountDetails)
     private readonly accountDetailsRepository: Repository<AccountDetails>,
+    @Inject(forwardRef(() => AccountService))
     private readonly accountService: AccountService,
   ) {}
+
+  async get(accountId: number): Promise<AccountDetails> {
+    const account = await this.accountService.findById(accountId);
+    if (account.accountDetails) {
+      return account.accountDetails;
+    } else {
+      throw new NotFoundException();
+    }
+  }
 
   async patch(
     accountId: number,
@@ -36,6 +51,20 @@ export class AccountDetailsService {
       return await this.accountService.patchAccount(findAccount);
     } else {
       throw new NotFoundException();
+    }
+  }
+  async saveAccountDetails(
+    accountDetails: AccountDetails,
+  ): Promise<AccountDetails> {
+    return await this.accountDetailsRepository.save(accountDetails);
+  }
+
+  async softRemove(accountDetailsId: number): Promise<void> {
+    const accountDetails = await this.accountDetailsRepository.findOne({
+      where: { id: accountDetailsId },
+    });
+    if (accountDetails) {
+      await this.accountDetailsRepository.remove(accountDetails);
     }
   }
 }
