aafc57b07bd946352f8fceed4a97ff43133a44dc
Daniel Obłąk <daniel.oblak@codeclusive.io>
2023-05-11 16:31:03 +0200

    Added Card typeormmodule import in accountModule, added controllers in AccountModule

--- src/accounts/account.module.ts
+++ src/accounts/account.module.ts
@@ -1,12 +1,14 @@
 import { AccountService } from './services/account.service';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AccountDetails } from 'src/account_details/models/account_details.model';
 import { Account } from './model/account.model';
+import { AccountController } from './controllers/account.controller';
+import { AccountDetails } from 'src/account_details/models/account_details.model';
+import { Card } from 'src/card/models/card.model';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Account, AccountDetails])],
-  controllers: [],
+  imports: [TypeOrmModule.forFeature([Account, Card, AccountDetails])],
+  controllers: [AccountController],
   providers: [AccountService],
   exports: [AccountService],
 })
