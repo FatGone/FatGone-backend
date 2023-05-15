305fd2b531983de4ad87bc8497e33d8b884dfd9d
Daniel <daniel.oblak99@gmail.com>
2023-05-15 22:41:00 +0200

    After user registration sendgrid will send him email

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,16 +1,24 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  Inject,
+  Injectable,
+  UnauthorizedException,
+  forwardRef,
+} from '@nestjs/common';
 import { compare, hash } from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 import { jwtTokenDTO } from '../models/jwt_token.dto';
 import { JwtPayload } from '../models/jwt_payload.dto';
 import { RegisterDto } from '../models/register.dto';
 import { AccountService } from 'src/accounts/services/account.service';
+import { SendGridService } from 'src/sendgrid/services/sendgrid.service';
 
 @Injectable()
 export class AuthService {
   constructor(
     private accountService: AccountService,
     private jwtService: JwtService,
+    @Inject(forwardRef(() => SendGridService))
+    private readonly sendGridService: SendGridService,
   ) {}
 
   async login(email: string, password: string): Promise<jwtTokenDTO> {
@@ -28,7 +36,15 @@ export class AuthService {
   async register(registerDto: RegisterDto): Promise<jwtTokenDTO> {
     const plainPassword = registerDto.password;
     registerDto.password = await hash(registerDto.password, 11);
-    await this.accountService.create(registerDto);
+    const response = await this.accountService.create(registerDto);
+    if (response) {
+      const mail = {
+        to: response.email,
+        from: 'dominik.szpilski@edgarsz.pl',
+        templateId: 'd-7d102b25cfb44207908a6c252e7e9b10',
+      };
+      await this.sendGridService.send(mail);
+    }
     return this.login(registerDto.email, plainPassword);
   }
 }
