aead6d65837283199949660e5746a3a27e80faad
Daniel <daniel.oblak99@gmail.com>
2023-05-16 20:47:51 +0200

    Early implementation of methods for mail sending in SendGridService

--- src/sendgrid/services/sendgrid.service.ts
+++ src/sendgrid/services/sendgrid.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotImplementedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import * as SendGrid from '@sendgrid/mail';
 
@@ -7,9 +7,31 @@ export class SendGridService {
   constructor(private readonly configService: ConfigService) {
     SendGrid.setApiKey(this.configService.get<string>('SEND_GRID_KEY'));
   }
+  async sendEmailConfirmationCode(code: string, email: string) {
+    const mail = {
+      to: email,
+      from: VERIFIED_SENDER,
+      templateId: 'not-implemented',
+    };
+    throw new NotImplementedException();
+  }
 
-  async send(mail: SendGrid.MailDataRequired) {
+  async sendPostRegisterMail(email: string) {
+    const mail = {
+      to: email,
+      from: VERIFIED_SENDER,
+      templateId: 'd-7d102b25cfb44207908a6c252e7e9b10',
+    };
     const transport = await SendGrid.send(mail);
     return transport;
+  }
+
+  async sendPasswordResetCode(code: string, email: string) {
+    const mail = {
+      to: email,
+      from: VERIFIED_SENDER,
+      templateId: 'not-implemented',
+    };
+    throw new NotImplementedException();
   }
 }
