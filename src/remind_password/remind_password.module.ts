780a33fae70a090a2ab708b8916848ef3f6ed622
Daniel <daniel.oblak99@gmail.com>
2023-05-17 23:15:36 +0200

    Added imports for SendGridModule and AccountModule in RemindPasswordModule

--- src/remind_password/remind_password.module.ts
+++ src/remind_password/remind_password.module.ts
@@ -1,10 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { RemindPassword } from './models/remind_password.model';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RemindPasswordService } from './services/remind_password.service';
+import { SendGridModule } from 'src/sendgrid/sendgrid.module';
+import { AccountModule } from 'src/accounts/account.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([RemindPassword])],
+  imports: [
+    TypeOrmModule.forFeature([RemindPassword]),
+    forwardRef(() => SendGridModule),
+    forwardRef(() => AccountModule),
+  ],
   controllers: [],
   providers: [RemindPasswordService],
   exports: [RemindPasswordService],
