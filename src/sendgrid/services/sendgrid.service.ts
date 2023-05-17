da777118d89867992f27bcbe5145d23c67e884d9
Daniel <daniel.oblak99@gmail.com>
2023-05-17 23:16:54 +0200

    Added dynamicTemplateData for SendPasswordResetCode method and templateId in SendGridService

--- src/sendgrid/services/sendgrid.service.ts
+++ src/sendgrid/services/sendgrid.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotImplementedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import * as SendGrid from '@sendgrid/mail';
+import { VERIFIED_SENDER } from 'src/core/utils/verified_sender';
 
 @Injectable()
 export class SendGridService {
@@ -22,16 +23,16 @@ export class SendGridService {
       from: VERIFIED_SENDER,
       templateId: 'd-7d102b25cfb44207908a6c252e7e9b10',
     };
-    const transport = await SendGrid.send(mail);
-    return transport;
+    return await SendGrid.send(mail);
   }
 
-  async sendPasswordResetCode(code: string, email: string) {
+  async sendPasswordResetCode(code: number, email: string) {
     const mail = {
       to: email,
       from: VERIFIED_SENDER,
-      templateId: 'not-implemented',
+      dynamic_template_data: { code: code },
+      templateId: 'd-f397ef6a2f75498d9d931add0658ce8d',
     };
-    throw new NotImplementedException();
+    return await SendGrid.send(mail);
   }
 }
