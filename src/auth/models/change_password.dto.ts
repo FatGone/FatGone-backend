3001e2d0a81b5175fa69040e9004eef8796f95f2
Daniel <daniel.oblak99@gmail.com>
2023-05-17 23:14:36 +0200

    Implemented ChangePasswordDto

---
+++ src/auth/models/change_password.dto.ts
@@ -0,0 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger/dist/decorators/api-property.decorator';
+import * as classValidator from 'class-validator';
+
+export class ChangePasswordDto {
+  @ApiProperty()
+  @classValidator.IsEmail()
+  @classValidator.MaxLength(45)
+  email: string;
+
+  @ApiProperty()
+  @classValidator.IsString()
+  @classValidator.Length(6, 60)
+  @classValidator.Matches(
+    /((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/,
+    {
+      message:
+        'Password should contain at least one capital letter and one special character.',
+    },
+  )
+  password: string;
+
+  @ApiProperty()
+  @classValidator.IsNumber()
+  code: number;
+}
