c2e019e6a1e07e477ef8f309a55c2103d5905560
Daniel <daniel.oblak99@gmail.com>
2023-05-31 05:45:54 +0200

    Removed validation from cardDto

--- src/card/dto/card.dto.ts
+++ src/card/dto/card.dto.ts
@@ -1,14 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsInt, IsString, Length, MaxLength } from 'class-validator';
+import { IsString, Length, MaxLength } from 'class-validator';
 
 export class CardDto {
   @ApiProperty()
   @IsString()
-  @Length(19, 19)
+  @Length(8, 19)
   cardNumber: string;
 
   @ApiProperty()
-  @IsInt()
   cvvNumber: number;
 
   @ApiProperty({ type: 'string', format: 'date-time' })
